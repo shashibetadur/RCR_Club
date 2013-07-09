572d0b64646a9e18b3444071d850d48970a25c8c
unknown <I044377@.(none)>
2013-07-09 21:41:20 +0530

    [Shashi] - home screen enhancement

--- code/Web/src/main/java/com/rcr/web/controller/HomeController.java
+++ code/Web/src/main/java/com/rcr/web/controller/HomeController.java
@@ -1,19 +1,61 @@
 package com.rcr.web.controller;
 
 
-import com.rcr.domain.Authorize;
-import com.rcr.domain.Operation;
+import com.rcr.domain.*;
+import com.rcr.service.bill.MemberBillService;
+import com.rcr.service.order.PurchaseOrderService;
+import com.rcr.web.model.MemberBillForm;
+import com.rcr.web.model.ProcessOrderForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class HomeController {
 
+    private MemberBillService memberBillService;
+    private PurchaseOrderService purchaseOrderService;
+
+    @Autowired
+    public HomeController(MemberBillService memberBillService, PurchaseOrderService purchaseOrderService) {
+        this.memberBillService = memberBillService;
+        this.purchaseOrderService = purchaseOrderService;
+    }
+
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     @Authorize(value = {Operation.HOME_PAGE})
-    public String addGroup() {
-        return "home";
+    public ModelAndView addGroup() {
+        BillSearchCriteria billSearchCriteria = new BillSearchCriteria();
+        OrderSearchCriteria orderSearchCriteria = new OrderSearchCriteria();
+
+        billSearchCriteria.getBillStatusList().add("NEW");
+        List<Bill> billList = memberBillService.search(billSearchCriteria);
+        List<MemberBillForm> memberBillList = new ArrayList<MemberBillForm>();
+        for (Bill bill : billList) {
+            MemberBillForm memberBillForm = new MemberBillForm();
+            memberBillForm.buildDisplayBill(bill);
+            memberBillList.add(memberBillForm);
+        }
+
+        orderSearchCriteria.getOrderStatusList().add('P');
+        orderSearchCriteria.getOrderStatusList().add('F');
+        List<PurchaseOrder> orderList = purchaseOrderService.search(orderSearchCriteria);
+        List<ProcessOrderForm> orderFormList = new ArrayList<ProcessOrderForm>();
+        for(PurchaseOrder order: orderList)  {
+            ProcessOrderForm processOrderForm = new ProcessOrderForm();
+            processOrderForm.buildDisplayOrder(order);
+            orderFormList.add(processOrderForm);
+        }
+
+        ModelAndView modelAndView = new ModelAndView("home", "memberBillList", memberBillList);
+        modelAndView.getModelMap().put("orderFormList", orderFormList);
+
+        return modelAndView;
     }
 }
--- code/Web/src/main/webapp/views/home.jsp
+++ code/Web/src/main/webapp/views/home.jsp
@@ -2,6 +2,43 @@
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib prefix="tiles" uri="http://tiles.apache.org/tags-tiles" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
-<div class="well">
+<div class="row well">
     <p>Welcome to RCR Raichur!!!</p>
 </div>
+ <div class="row well">
+    <legend>Action Items</legend>
+    <div class="info-details">
+        <div class="span12 nk-form-section">
+            <div class="accordion" id="info-details-accordion">
+
+                <div class="accordion-group">
+                    <div class="accordion-heading">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="new-bills-accordion"
+                           href="#collapse-new-bills"><h5>New Bills</h5></a>
+                    </div>
+                    <div id="collapse-new-bills" class="accordion-body collapse">
+                        <div class="accordion-inner">
+                            <jsp:include page="bill/searchResults.jsp"/>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="accordion-group">
+                    <div class="accordion-heading">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="pos-accordion"
+                           href="#collapse-pos"><h5>Purchase Orders</h5></a>
+                    </div>
+                    <div id="collapse-pos" class="accordion-body collapse">
+                        <div class="accordion-inner">
+                            <jsp:include page="order/searchResults.jsp"/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+ </div>
+<script type="text/javascript">
+    $(".accordion").collapse();
+</script>
